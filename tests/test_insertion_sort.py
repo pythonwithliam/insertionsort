143b16ce6d2dd8cf3862df05687cf457fdbd1a19
Python With Liam <pythonwithliam@gmail.com>
2020-03-30 18:01:28 -0500

    Fix to tests
    Add test and sorted array to each test to fix issue where the first test would modify the array, already sorting it for the next test

--- tests/test_insertion_sort.py
+++ tests/test_insertion_sort.py
@@ -2,18 +2,22 @@ import unittest
 import insertion_sort as ins
 
 class TestSuite(unittest.TestCase):
-    test_arr = [4, 2, 6, 3, 7, 8, 1, 9, 5]
-    sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
     def test_iterative_insertion_sort(self):
-        self.assertEqual(self.sorted_arr,
-                         ins.iterative_insertion_sort(self.test_arr))
+        test_arr = [4, 2, 6, 3, 7, 8, 1, 9, 5]
+        sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(sorted_arr,
+                         ins.iterative_insertion_sort(test_arr))
 
     def test_recursive_insertion_sort(self):
-        self.assertEqual(self.sorted_arr,
-                         ins.recursive_insertion_sort(self.test_arr,
-                                                      len(self.test_arr)))
+        test_arr = [4, 2, 6, 3, 7, 8, 1, 9, 5]
+        sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(sorted_arr,
+                         ins.recursive_insertion_sort(test_arr,
+                                                      len(test_arr)))
 
     def test_binary_insertion_sort(self):
-        self.assertEqual(self.sorted_arr,
-                         ins.binary_insertion_sort(self.test_arr))
+        test_arr = [4, 2, 6, 3, 7, 8, 1, 9, 5]
+        sorted_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(sorted_arr,
+                         ins.binary_insertion_sort(test_arr))
